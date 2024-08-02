ae4220c193bb6a086f8f06bbd8ed322e7e598fda
piuconal <hcboy1106@gmail.com>
2024-08-02 08:44:09 +0700

    View book list/details at the library

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,6 +5,18 @@ class Book < ApplicationRecord
   has_many :borrow_requests, dependent: :destroy
   has_many :comments, dependent: :destroy
 
+  scope :by_author, lambda {|author_id|
+    where(author_id:) if author_id.present?
+  }
+
+  scope :by_category, lambda {|category_id|
+    where(category_id:) if category_id.present?
+  }
+
+  scope :by_author_and_category, lambda {|author_id, category_id|
+    by_author(author_id).by_category(category_id)
+  }
+
   validates :title, presence: true
   validates :author_id, :publisher_id, :category_id, presence: true
   validates :description, presence: true
