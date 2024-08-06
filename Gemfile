880b85da5e4c7d1579b7a12527a8fc56f2b73c11
piuconal <hcboy1106@gmail.com>
2024-08-06 10:53:43 +0700

    Look for information

--- Gemfile
+++ Gemfile
@@ -32,6 +32,7 @@ gem "tzinfo-data", platforms: %i(mingw mswin x64_mingw jruby)
 gem "bootsnap", require: false
 gem "bootstrap-sass", "3.4.1"
 gem "config"
+gem "ransack"
 gem "sassc-rails"
 
 group :development, :test do
--- app/models/book.rb
+++ app/models/book.rb
@@ -5,22 +5,15 @@ class Book < ApplicationRecord
   has_many :borrow_requests, dependent: :destroy
   has_many :comments, dependent: :destroy
 
-  scope :by_author, lambda {|author_id|
-    where(author_id:) if author_id.present?
-  }
-
-  scope :by_category, lambda {|category_id|
-    where(category_id:) if category_id.present?
-  }
-
-  scope :by_author_and_category, lambda {|author_id, category_id|
-    by_author(author_id).by_category(category_id)
-  }
-
   validates :title, presence: true
   validates :author_id, :publisher_id, :category_id, presence: true
   validates :description, presence: true
   validates :published_date, presence: true, if: ->{published_date.present?}
   validates :rental_price, numericality: {greater_than_or_equal_to: 0},
              allow_nil: true
+
+  def self.ransackable_attributes _auth_object = nil
+    %w(author_id category_id created_at description id
+      published_date publisher_id rental_price title updated_at)
+  end
 end
